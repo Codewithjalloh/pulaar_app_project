6a314672b6e647da67248bb7ee199f368da241a7
codewithjalloh <codewithjalloh@gmail.com>
2024-06-13 21:07:19 +0100

    home screen page

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import '../components/custom_drawer.dart';
 import 'chat_screen.dart';
 import 'profile_screen.dart';
-import '../model/section.dart';
-import 'category_screen.dart';
 
 class HomePageScreen extends StatefulWidget {
   @override
@@ -21,13 +19,16 @@ class _HomePageScreenState extends State<HomePageScreen> {
 
   static List<Widget> _widgetOptions = <Widget>[
     HomePageContent(),
-    ChatPageWrapper(),
-    ProfilePageWrapper(),
+    ChatScreen(),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Pulaar"),
+      ),
       drawer: CustomDrawer(),
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
@@ -56,62 +57,51 @@ class _HomePageScreenState extends State<HomePageScreen> {
 class HomePageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pulaar"),
-      ),
-      body: ListView.builder(
-        itemCount: sections.length,
-        itemBuilder: (context, index) {
-          final section = sections[index];
-          return Column(
-            children: [
-              ListTile(
-                leading: Icon(section.icon, size: 50),
-                title: Text(section.title, style: TextStyle(fontSize: 25)),
-                subtitle:
-                    Text(section.subtitle, style: TextStyle(fontSize: 20)),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CategoryScreen(
-                        title: section.title,
-                        filename: section.filename,
-                      ),
-                    ),
-                  );
-                },
-              ),
-              Divider(),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ChatPageWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat'),
-      ),
-      body: ChatScreen(),
-    );
-  }
-}
-
-class ProfilePageWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: ProfileScreen(),
+    return ListView(
+      children: [
+        ListTile(
+          leading: Icon(Icons.history),
+          title: Text('Fulani History'),
+          onTap: () {
+            Navigator.pushNamed(context, '/fulani_history');
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.quiz),
+          title: Text('Quiz Section'),
+          onTap: () {
+            Navigator.pushNamed(context, '/quiz_section');
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.money),
+          title: Text('Currency Converter'),
+          onTap: () {
+            Navigator.pushNamed(context, '/currency_converter');
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.public),
+          title: Text('African Countries'),
+          onTap: () {
+            Navigator.pushNamed(context, '/african_countries');
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.language),
+          title: Text('Must Know Words'),
+          onTap: () {
+            Navigator.pushNamed(context, '/must_know_words');
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.info),
+          title: Text('Information'),
+          onTap: () {
+            Navigator.pushNamed(context, '/information');
+          },
+        ),
+      ],
     );
   }
 }
