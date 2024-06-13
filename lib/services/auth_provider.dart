3e3804a33e2c10b467398606d9b1119124f7ad6f
codewithjalloh <codewithjalloh@gmail.com>
2024-06-13 21:06:35 +0100

    auth provider added

---
+++ lib/services/auth_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  AuthProvider() {
+    _auth.authStateChanges().listen((user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  User? get user => _user;
+
+  bool get isLoggedIn => _user != null;
+
+  Future<void> login(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
