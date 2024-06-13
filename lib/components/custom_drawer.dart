0bde6ca2052d2670051be39e54ae3f031e548056
codewithjalloh <codewithjalloh@gmail.com>
2024-06-13 21:05:52 +0100

    custom drawer updated

--- lib/components/custom_drawer.dart
+++ lib/components/custom_drawer.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import '../services/auth_provider.dart';
 import '../screens/login_screen.dart';
+import '../screens/chat_screen.dart';
+import '../screens/profile_screen.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final User? user = FirebaseAuth.instance.currentUser;
+    final AuthProvider authProvider = Provider.of<AuthProvider>(context);
 
     return Drawer(
       child: ListView(
@@ -33,99 +37,55 @@ class CustomDrawer extends StatelessWidget {
               Navigator.pushNamed(context, '/');
             },
           ),
-          _buildDrawerItem(
-            context,
-            icon: Icons.history,
-            text: 'Fulani History',
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/fulani_history');
-            },
-          ),
-          _buildDrawerItem(
-            context,
-            icon: Icons.quiz,
-            text: 'Quiz Section',
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/quiz_section');
-            },
-          ),
-          _buildDrawerItem(
-            context,
-            icon: Icons.money,
-            text: 'Currency Converter',
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/currency_converter');
-            },
-          ),
-          _buildDrawerItem(
-            context,
-            icon: Icons.public,
-            text: 'African Countries',
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/african_countries');
-            },
-          ),
-          _buildDrawerItem(
-            context,
-            icon: Icons.language,
-            text: 'Must Know Words',
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushNamed(context, '/must_know_words');
-            },
-          ),
-          if (user != null)
+          if (authProvider.isLoggedIn) ...[
             _buildDrawerItem(
               context,
               icon: Icons.chat,
               text: 'Chat',
               onTap: () {
+                Navigator.pop(context);
                 Navigator.pushNamed(context, '/chat');
               },
             ),
-          if (user != null)
             _buildDrawerItem(
               context,
-              icon: Icons.person,
+              icon: Icons.account_circle,
               text: 'Profile',
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, '/profile');
               },
             ),
-          if (user == null)
+            _buildDrawerItem(
+              context,
+              icon: Icons.logout,
+              text: 'Logout',
+              onTap: () {
+                authProvider.logout();
+              },
+            ),
+          ] else ...[
             _buildDrawerItem(
               context,
               icon: Icons.login,
               text: 'Login',
               onTap: () {
+                Navigator.pop(context);
                 Navigator.pushNamed(context, '/login');
               },
             ),
-          if (user != null)
-            _buildDrawerItem(
-              context,
-              icon: Icons.logout,
-              text: 'Logout',
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pop(context);
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
+          ],
         ],
       ),
     );
   }
 
-  Widget _buildDrawerItem(BuildContext context,
-      {required IconData icon,
-      required String text,
-      required VoidCallback onTap}) {
+  Widget _buildDrawerItem(
+    BuildContext context, {
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
     return ListTile(
       leading: Icon(icon, size: 30),
       title: Text(
