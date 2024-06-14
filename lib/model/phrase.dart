a7626bb797866c3b13b6c73463185269225bd56f
codewithjalloh <codewithjalloh@gmail.com>
2024-06-14 19:38:28 +0100

    phrase updated

--- lib/model/phrase.dart
+++ lib/model/phrase.dart
@@ -2,7 +2,10 @@ class Phrase {
   final String english;
   final String pulaar;
 
-  Phrase({required this.english, required this.pulaar});
+  Phrase({
+    required this.english,
+    required this.pulaar,
+  });
 
   factory Phrase.fromJson(Map<String, dynamic> json) {
     return Phrase(
@@ -17,15 +20,4 @@ class Phrase {
       'pulaar': pulaar,
     };
   }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Phrase &&
-          runtimeType == other.runtimeType &&
-          english == other.english &&
-          pulaar == other.pulaar;
-
-  @override
-  int get hashCode => english.hashCode ^ pulaar.hashCode;
 }
