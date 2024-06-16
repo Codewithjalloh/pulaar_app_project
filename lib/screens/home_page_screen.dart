65598d97d01338ddf89678a89f926beb966b3f70
codewithjalloh <codewithjalloh@gmail.com>
2024-06-16 12:23:44 +0100

    home page update

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -1,30 +1,9 @@
 import 'package:flutter/material.dart';
 import '../components/custom_drawer.dart';
-import '../model//sections.dart';
-import 'category_screen.dart';
-import '../screens/profile_screen.dart';
-import '../screens/chat_screen.dart';
+import '../model/section.dart';
+import '../screens/phrase_detail_screen.dart';
 
-class HomePageScreen extends StatefulWidget {
-  @override
-  _HomePageScreenState createState() => _HomePageScreenState();
-}
-
-class _HomePageScreenState extends State<HomePageScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  static List<Widget> _widgetOptions = <Widget>[
-    HomePageContent(),
-    ChatPageWrapper(),
-    ProfilePageWrapper(),
-  ];
-
+class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,25 +11,27 @@ class _HomePageScreenState extends State<HomePageScreen> {
         title: Text("Pulaar"),
       ),
       drawer: CustomDrawer(),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
+      body: ListView.builder(
+        itemCount: sections.length,
+        itemBuilder: (context, index) {
+          final section = sections[index];
+          return ListTile(
+            leading: Icon(section.icon, size: 50),
+            title: Text(section.title, style: TextStyle(fontSize: 25)),
+            subtitle: Text(section.subtitle, style: TextStyle(fontSize: 20)),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PhraseDetailScreen(
+                    title: section.title,
+                    filename: section.filename,
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
@@ -63,60 +44,23 @@ class HomePageContent extends StatelessWidget {
       itemCount: sections.length,
       itemBuilder: (context, index) {
         final section = sections[index];
-        return Column(
-          children: [
-            ListTile(
-              leading: Icon(section.icon, size: 50),
-              title: Text(section.title, style: TextStyle(fontSize: 25)),
-              subtitle: Text(section.subtitle, style: TextStyle(fontSize: 20)),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CategoryScreen(
-                      title: section.title,
-                      filename: section.filename,
-                    ),
-                  ),
-                );
-              },
-            ),
-            Divider(),
-          ],
+        return ListTile(
+          leading: Icon(section.icon, size: 50),
+          title: Text(section.title, style: TextStyle(fontSize: 25)),
+          subtitle: Text(section.subtitle, style: TextStyle(fontSize: 20)),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PhraseDetailScreen(
+                  title: section.title,
+                  filename: section.filename,
+                ),
+              ),
+            );
+          },
         );
       },
-    );
-  }
-}
-
-class ChatPageWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat'),
-      ),
-      body: ChatScreen(),
-    );
-  }
-}
-
-class ProfilePageWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              // Navigate to edit profile screen or enable edit mode
-            },
-          ),
-        ],
-      ),
-      body: ProfileScreen(),
     );
   }
 }
