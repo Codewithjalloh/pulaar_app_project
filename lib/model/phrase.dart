6c99b5ec9030be367770cfb7e61250db07a5d1ab
codewithjalloh <codewithjalloh@gmail.com>
2024-06-24 19:15:06 +0100

    Phrase updated

--- lib/model/phrase.dart
+++ lib/model/phrase.dart
@@ -1,20 +1,24 @@
 class Phrase {
-  final String english;
-  final String pulaar;
+  // Define the properties for the class
+  final String english; // The English version of the phrase
+  final String pulaar; // The Pulaar version of the phrase
 
+  // Constructor for initializing the properties
   Phrase({required this.english, required this.pulaar});
 
+  // Factory constructor for creating a new Phrase instance from a JSON map
   factory Phrase.fromJson(Map<String, dynamic> json) {
     return Phrase(
-      english: json['english'],
-      pulaar: json['pulaar'],
+      english: json['english'], // Extract the English phrase from the JSON map
+      pulaar: json['pulaar'], // Extract the Pulaar phrase from the JSON map
     );
   }
 
+  // Method for converting a Phrase instance into a JSON map
   Map<String, dynamic> toJson() {
     return {
-      'english': english,
-      'pulaar': pulaar,
+      'english': english, // Add the English phrase to the JSON map
+      'pulaar': pulaar, // Add the Pulaar phrase to the JSON map
     };
   }
 }
