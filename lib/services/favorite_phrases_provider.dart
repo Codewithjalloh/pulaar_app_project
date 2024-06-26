c39cae62a71d86d1d6496731140f691d4480841e
codewithjalloh <codewithjalloh@gmail.com>
2024-06-26 23:15:21 +0100

    fav screeen update updated

--- lib/services/favorite_phrases_provider.dart
+++ lib/services/favorite_phrases_provider.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import '../model/phrase.dart';
 
 class FavoritePhrasesProvider extends ChangeNotifier {
+  // List to hold favorite phrases
   List<Phrase> _favoritePhrases = [];
 
+  // Getter to access the list of favorite phrases
   List<Phrase> get favoritePhrases => _favoritePhrases;
 
+  // Method to add or remove a phrase from favorites
   void toggleFavorite(Phrase phrase) {
+    // Check if the phrase is already in favorites
     if (_favoritePhrases.contains(phrase)) {
+      // Remove the phrase from favorites
       _favoritePhrases.remove(phrase);
     } else {
+      // Add the phrase to favorites
       _favoritePhrases.add(phrase);
     }
+    // Notify listeners about the change
     notifyListeners();
   }
 
+  // Method to check if a phrase is in favorites
   bool isFavorite(Phrase phrase) {
     return _favoritePhrases.contains(phrase);
   }
