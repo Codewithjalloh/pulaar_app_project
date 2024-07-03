550fc1d63e3e6094bf65e86bfe76af857c955b4d
codewithjalloh <codewithjalloh@gmail.com>
2024-07-03 20:38:13 +0100

    CustomDrawer updated

--- lib/components/custom_drawer.dart
+++ lib/components/custom_drawer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../screens/favorites_screen.dart';
 
 class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({Key? key}) : super(key: key);
+  CustomDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
